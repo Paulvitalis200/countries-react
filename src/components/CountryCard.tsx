b532aeb188ed67195e68f211819f550fb23dc7b7
Paul Otieno <vitalispaul48@live.com>
2024-03-24 11:27:31 +0300

    Update cards

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -8,8 +8,6 @@ interface CountryProps {
 const CountryCard = ({ country }: CountryProps) => {
   return (
     <Card
-      maxW="264px"
-      width="264px"
       height="336px"
       marginBottom="20px"
       borderRadius="5px"
